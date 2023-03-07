eaaee4fc3ae4dde9c164197fb4f6f8e305c53af6
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-03-07 13:17:32 +0100

    Generated resource auth for login

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WorkersModule } from './workers/workers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, WorkersModule],
+  imports: [UsersModule, WorkersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
