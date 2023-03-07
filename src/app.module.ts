c4831009c9784b79157fde9edfd823c24cd37a26
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-03-07 13:16:30 +0100

    Generated resource workers for team members

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { WorkersModule } from './workers/workers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, WorkersModule],
   controllers: [AppController],
   providers: [AppService],
 })
