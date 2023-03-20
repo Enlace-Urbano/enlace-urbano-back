9c16fa9042f8236e0473370c263de4c13c1b808d
ChaanChanChan <alexandertakepics@gmail.com>
2023-03-20 17:20:48 +0100

    class-validator installed, only letters allowed on username and password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/)
   readonly username: string;
 
   @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/)
   password: string;
 }
