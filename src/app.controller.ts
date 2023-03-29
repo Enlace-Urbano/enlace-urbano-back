4b7e2c6448b48e098d74bcf2b53362db00c48d5e
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-03-29 13:02:06 +0200

    first steps fixing auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+
 
 @Controller()
 export class AppController {
@@ -17,9 +15,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
