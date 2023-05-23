64f397f5035eed1bcdc609ad5cad86d2467d631f
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-05-23 21:14:34 +0200

    fixed swagger dto's, removed login from swagger

--- src/auth/login.controller.ts
+++ src/auth/login.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+// import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+// import { ApiTags } from '@nestjs/swagger';
+// import { AuthService } from './auth.service';
+// import { LocalAuthGuard } from './local-auth.guard';
 
-@ApiTags('login')
-@Controller()
-export class LoginController {
-  constructor(private authService: AuthService) {}
+// @ApiTags('login')
+// @Controller()
+// export class LoginController {
+//   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Body() user: any) {
-    console.log(user);
-    return this.authService.login(user);
-  }
-}
+//   @UseGuards(LocalAuthGuard)
+//   @Post('auth/login')
+//   async login(@Body() user: any) {
+        
+//     console.log(user);
+//     return this.authService.login(user);
+//   }
+// }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,13 +2,13 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Matches } from 'class-validator';
 
 export class CreateUserDto {
-  // @ApiProperty({ required: true })
-  // @IsNotEmpty()
-  // @Matches(/^[a-zA-Z]+$/)
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/)
   readonly username: string;
 
-  // @ApiProperty({ required: true })
-  // @IsNotEmpty()
-  // @Matches(/^[a-zA-Z]+$/)
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/)
   password: string;
 }
